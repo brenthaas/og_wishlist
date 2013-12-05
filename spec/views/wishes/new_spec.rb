2aa9d515fbdd8f653390742e7556a6091e1fba09
Brent Haas <brenthaas@gmail.com>
2013-12-04 21:56:44 -0800

    [FEATURE] Add hidden user_id field to new form

---
+++ spec/views/wishes/new_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe "/wishes/new.html.erb" do
+  context "when a user is logged in" do
+    before (:each) do
+      assign(:wish, stub_model(Wish) )
+      @user = stub_model(User)
+      view.stub(:current_user).and_return @user
+    end
+    it "makes a hidden field with the user id" do
+      render
+      expect(rendered).to match /<input id="user_id".*type="hidden" value="\d+"/
+    end
+  end
+end
