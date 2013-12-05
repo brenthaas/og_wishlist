8f4eb89e2944524c301717982602c3f0e5132c8a
Brent Haas <brenthaas@gmail.com>
2013-12-04 23:46:22 -0800

    [UPDATE] Added factory for a wish

--- spec/factories.rb
+++ spec/factories.rb
@@ -5,4 +5,11 @@ FactoryGirl.define do
     password "password"
     password_confirmation "password"
   end
+
+  factory :wish do
+    title "My two front teeth"
+    summary "This is a description"
+    url "http://google.com"
+    image "http://google.com/image.jpg"
+  end
 end
