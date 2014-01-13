86581eb73927259eea9d1507b6a5bb063fee7fc2
Brent Haas <brenthaas@gmail.com>
2014-01-12 17:32:41 -0800

    [UPDATE] Added user -> wish associations

--- spec/factories.rb
+++ spec/factories.rb
@@ -8,9 +8,18 @@ FactoryGirl.define do
     email  { FactoryGirl.generate :email }
     password "password"
     password_confirmation "password"
+    factory :user_with_wishes do
+      ignore do
+        wish_count 2
+      end
+      after(:create) do |user, evaluator|
+        create_list(:wish, evaluator.wish_count, user: user)
+      end
+    end
   end
 
   factory :wish do
+    user
     title "My two front teeth"
     summary "This is a description"
     url "http://google.com"
